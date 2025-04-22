1488f64d7057b2f05bc72b8e81c063829d3090f3
Katia Martins <katiamartins13@yahoo.com.br>
2025-04-22 15:27:21 -0300

    feat(p1): exibe localidades reais

--- busca-cep/src/App.jsx
+++ busca-cep/src/App.jsx
@@ -7,14 +7,15 @@ class App extends Component {
 
   state = {
     termoDeBusca: '',
+    localidade: []
   }
 
   buscarLocalidade = (cep) => {
     this.setState({ termoDeBusca: cep })
   }
 
-  onBuscarClick = (cep) => { 
-    
+  onBuscarClick = (cep) => {
+
     if (cep === '') {
       alert('Busca vazia. Insira um CEP.')
       return
@@ -24,38 +25,40 @@ class App extends Component {
       alert('Insira um CEP com 8 dígitos.')
       return
     }
+   
+    axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((response) => {
+        const objetoRetorno = response.data
+        if (objetoRetorno.erro) {
+          alert('CEP não localizado na base.')
+          console.log(objetoRetorno)
+          return
+        }
+        this.setState((state) => ({
+          localidade: [objetoRetorno].concat(state.localidade)
+        })) 
+      })
+      .catch((error) => {
+        console.error(error)
+        alert('Ocorreu um erro ao realizar a busca.')
+      })
+  }
 
-    axios.get (`https://viacep.com.br/ws/${cep}/json/`)
-    .then((response) => {
-      const objetoRetorno = response.data
-      if(objetoRetorno.erro){
-        alert('CEP não localizado na base.')
-        console.log(objetoRetorno)
-        return
-      }
-      console.log(objetoRetorno)
-    })
-    .catch((error) => {
-      console.error(error)
-      alert('Ocorreu um erro ao realizar a busca.')
-    })      
-  }   
-  
   render() {
-  return (
-    <div className="grid justify-content-center">
-      <div className="col-12">
-         <h1 className="text-center">Buscador de CEPs</h1>
+    return (
+      <div className="grid justify-content-center">
+        <div className="col-12">
+          <h1 className="text-center">Buscador de CEPs</h1>
+        </div>
+        <div className="col-12">
+          <Busca
+            dica='Informe o CEP com 8 dígitos'
+            onBuscarClick={this.onBuscarClick} />
+        </div>
+        <div className="col-12">
+          <LocalidadeLista localidade={this.state.localidade} />
+        </div>
       </div>
-      <div className="col-12">
-        <Busca 
-        dica='Informe o CEP com 8 dígitos'
-        onBuscarClick={this.onBuscarClick} />
-      </div>
-      <div className="col-12">
-        <LocalidadeLista cep={this.state.termoDeBusca} />
-      </div>
-    </div>
     );
   }
 }
--- busca-cep/src/LocalidadeLista.jsx
+++ busca-cep/src/LocalidadeLista.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
 import { Card } from 'primereact/card'
 
-const LocalidadeLista = ({ cep }) => {
-  const localPorCEP = {
-    '04094050': { nome: 'Avenida Pedro Álvares Cabral', bairro: 'Parque Ibirapuera', cidade: 'São Paulo', estado: 'SP', cep: '04094-050' },
-    '55592970': { nome: 'Rua dos Navegantes', bairro: 'Vila de Porto de Galinhas', cidade: 'Ipojuca', estado: 'PE', cep: '55592-970' },
-    '': {}, 
-  };
-
-  const infoLocal = localPorCEP[cep]
+const LocalidadeLista = ({ localidade }) => {
 
   return (
     <div className="flex flex-column align-items-center justify-content-center w-full">
+      {localidade.map((info) => (
       <Card className="w-full">
-        <p className="m-0 text-center">{infoLocal.cep}</p>
-        <p className="m-0 text-center">{infoLocal.nome}</p>
-        <p className="m-0 text-center">{infoLocal.bairro}</p>
+        <p className="m-0 text-center">{info.cep}</p>
+        <p className="m-0 text-center">{info.logradouro}</p>
+        <p className="m-0 text-center">{info.bairro}</p>
         <p className="m-0 text-center">
-          {infoLocal.cidade} - {infoLocal.estado}
+          {info.localidade} - {info.uf}
         </p>
       </Card>
+      ))}
     </div>
   )
 }
